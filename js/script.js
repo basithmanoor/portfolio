b4188e1e998a48641aba45683eb52b6f3a8324ac
mohdshuhaib <mohdshuhaib357@gmail.com>
2024-02-28 22:06:24 +0530

    some editz

--- js/script.js
+++ js/script.js
@@ -10,4 +10,31 @@ function filterWorks(category) {
             item.style.display = 'none';
         }
     });
+
+    // Remove the "active" class from all buttons
+    let buttons = document.querySelectorAll('.filter-buttons button');
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+
+    // Add the "active" class to the clicked button
+    let activeButton = document.querySelector(`.filter-buttons button[data-category="${category}"]`);
+    activeButton.classList.add('active');
+};
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTopButton").style.display = "block";
+    } else {
+        document.getElementById("scrollToTopButton").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
